15305e974c76026813d0adc021d471b8d7fe61a8
tpauldike <topman4loveworld@gmail.com>
2023-12-09 09:24:42 +0000

    chore(more styling): making the web page to be mobile responsive

---
+++ client-side/scripts/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuToggle = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+  
+    mobileMenuToggle.addEventListener('click', function () {
+      navList.classList.toggle('show');
+    });
+  });
