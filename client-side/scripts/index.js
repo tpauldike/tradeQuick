e341340e0f654c6d4efa9df321e80f267eb70aeb
tpauldike <topman4loveworld@gmail.com>
2023-12-15 01:42:18 +0000

    feat(landing-page): completed the CSS styling of sessions 1 & 3

--- client-side/scripts/index.js
+++ client-side/scripts/index.js
@@ -1,10 +1,47 @@
+const icons = [{
+  htmlClass: '.likes',
+  imgSrc: './assets/like.png'
+},
+{
+  htmlClass: '.dislikes',
+  imgSrc: './assets/dislike.png'
+},
+{
+  htmlClass: '.add-to-cart',
+  imgSrc: './assets/add-to-cart.png'
+},
+{
+  htmlClass: '.comment',
+  imgSrc: './assets/comment.png'
+},
+{
+  htmlClass: '.ellipse',
+  imgSrc: './assets/ellipse.png'
+}
+];
 
+const displaySocialInteractions = (htmlClass, imgSrc) => {
+  const htmlClassList = document.querySelectorAll(htmlClass);
 
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuToggle = document.getElementById('mobile-menu');
-    const navList = document.querySelector('.nav-list');
-  
-    mobileMenuToggle.addEventListener('click', function () {
-      navList.classList.toggle('show');
-    });
+  htmlClassList.forEach(htmlElement => {
+    const img = document.createElement('img');
+    img.src = imgSrc;
+    htmlElement.appendChild(img);
   });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  icons.map((icon) => {
+    displaySocialInteractions(icon.htmlClass, icon.imgSrc)
+  })
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const mobileMenuToggle = document.getElementById('mobile-menu');
+//   const navList = document.querySelector('.nav-list');
+
+//   mobileMenuToggle.addEventListener('click', function () {
+//     navList.classList.toggle('show');
+//   });
+// });
