23d4bd87129cc15285f86d2e0c262c8fe80e7be1
davidgregs87 <udohdavid27@gmail.com>
2023-12-27 14:23:39 +0100

    chore(API DOCUMENTATION): made changes readme documentation

--- server-side/api/v1/views/users.py
+++ server-side/api/v1/views/users.py
@@ -6,7 +6,20 @@ from flask import abort, jsonify, request, abort
 import os
 from uuid import uuid4
 from bcrypt import hashpw, gensalt, checkpw
+import cloudinary
+from dotenv import load_dotenv
 
+load_dotenv()
+
+CLOUDNAME = os.getenv("CLOUD_NAME")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+
+cloudinary.config(
+    cloud_name = CLOUDNAME
+    api_key = API_KEY
+    api_secret = API_SECRET
+)
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
